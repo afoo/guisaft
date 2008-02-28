c6d78a354232a64afbbadb91b8f05417be5b7fa5
afoo42 <afoo42@155c56e2-3e47-0410-a15b-2d497c942a6d>
2008-02-28 23:28:32 +0000

    * made saft/ work as a module by adding __init__.py * moddified guisaft.* to display first simple gui

--- guisaft.py
+++ guisaft.py
@@ -2,9 +2,30 @@
 #
 # GPL-3 ble
 
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gtk.glade
+
+
+class GuiSaftApp(object):
+
+    GLADEFILE = './guisaft.glade'
+
+    def __init__(self):
+        self.glade = gtk.glade.XML(self.GLADEFILE)
+        self.window = self.glade.get_widget('window1')
+        self.window.connect('destroy', gtk.main_quit)
+        self.glade.signal_autoconnect({
+                'on_applyButton_clicked': self.onApplyClick,
+                })
+                
+    def onApplyClick(self, widget):
+        print 'apply button clicked!'
 
 def main():
-    pass
+    app = GuiSaftApp()
+    gtk.main()
 
 if __name__ == '__main__':
     main()
