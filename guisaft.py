74f31a56e4a25fac57956212bc361e91da5c1b7a
afoo42 <afoo42@155c56e2-3e47-0410-a15b-2d497c942a6d>
2008-02-29 15:59:34 +0000

    username, fromaddr, some validation

--- guisaft.py
+++ guisaft.py
@@ -14,38 +14,73 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+
+# major TODO: add i18n
+
 import pygtk
 pygtk.require('2.0')
 import gtk
 import gtk.glade
 
+try:
+    import win32api
+except ImportError:
+    have_win32api = False
+else:
+    have_win32api = True
+
+import sys
+import os
+import socket
+
+from saft.client import SaftClient
 
 class GuiSaftApp(object):
 
-    GLADEFILE = './guisaft.glade'
+    MAINWIN_GLADEFILE = './guisaft.glade'
+    SENDWIN_GLADEFILE = ''
 
     def __init__(self):
-        self.glade = gtk.glade.XML(self.GLADEFILE)
+        self.glade = gtk.glade.XML(self.MAINWIN_GLADEFILE)
         self.window = self.glade.get_widget('window1')
         self.filechooser = self.glade.get_widget('filechooserbutton1')
+        self.toEntry = self.glade.get_widget('toEntry') # TODO: actually change the control to have that name
         self.window.connect('destroy', gtk.main_quit)
         self.glade.signal_autoconnect({
                 'on_applyButton_clicked': self.onApplyClick,
                 'on_filechooserbutton1_file_set': self.onFileSet,
                 })
         self.filename = None
+        username = self.getUserName()
+        hostname = socket.gethostname()
+        self.fromaddr = '%s@%s' % (username, hostname)
+
+    def getUserName(self):
+        if have_win32api:
+            return win32api.GetUserName()
+        else:
+            return os.environ.get('USER', 'unknown')
                 
     def onApplyClick(self, widget):
         print 'apply button clicked!'
+        toaddr = self.toEntry.get_text()
         if self.filename is None:
             self.showError('no file selected')
+        elif not os.path.isfile(self.filename):
+            self.showError('%s is not a valid file' % self.filename)
+        elif toaddr = '' or '@' not in toaddr:
+            self.showError('no (valid) recipient address given')
         else:
-            print self.filename
-
+            # TODO figure out if multithreading is to be handled here or directly in saft.client
+            sc = SaftCient(self.fromaddr, toaddr, self.filename, self.progressCallback)
+            
     def onFileSet(self, widget):
         self.filename = widget.get_filename()
 
+    def progressCallback(self, progress):
+        # update progressbar
+        pass
+    
     def showError(self, msg):
         dialog = gtk.MessageDialog(self.window, gtk.DIALOG_MODAL, 
                                    gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, msg)
