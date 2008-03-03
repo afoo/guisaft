ba237f45d66982b50d8a80b1a2650e298f8c53bf
afoo42 <afoo42@155c56e2-3e47-0410-a15b-2d497c942a6d>
2008-03-03 19:43:34 +0000

    sending over gui works now, progress bar update not though

--- trunk/src/saft/client.py
+++ trunk/src/saft/client.py
@@ -19,6 +19,11 @@
 import socket
 import os
 
+DEBUG = True
+def dprint(*a):
+    if DEBUG:
+        print a
+
 class SaftClient(object):
 
     __slots__ = (
@@ -27,16 +32,16 @@ class SaftClient(object):
         'filename', # str
         'fileobj', # file
         'filesize', # int (bytes)
-        'progress_callback', # func
+        'progress_callbacks', # list of functions
         'compress', # bool
         'sock', # socket
         )
 
-    def __init__(self, fromaddr, toaddr, filename, prog_cb, compress=True):
+    def __init__(self, fromaddr, toaddr, filename, compress=True):
         self.fromaddr = fromaddr
         self.toaddr = toaddr
         self.filename = filename
-        self.progress_callback = prog_cb
+        self.progress_callbacks = list()
         self.compress = compress
 
         self.fileobj = open(filename, 'r')
@@ -44,35 +49,45 @@ class SaftClient(object):
   
         self.sock = socket.socket()
 
+    def addCallback(self, callback):
+        self.progress_callbacks.append(callback)
+
+    def executeCallbacks(self, progress):
+        for cb in self.progress_callbacks:
+            if not cb(progress):
+                return False
+        return True
+
     def send(self):
+        if len(self.progress_callbacks) == 0:
+            raise 'some exception'
         user, host = self.toaddr.split('@') # evtl doch seperat reingeben?
         self.sock.connect((host, 487))
-        self.sock.send("FROM %s \r\n" % self.fromaddr) 
-        print self.sock.recv(1024)
-        self.sock.send("TO %s \r\n" % user)
-        print self.sock.recv(1024) 
-        self.sock.send("FILE %s \r\n" % self.filename) 
-        print self.sock.recv(1024)
+        self.sock.send("FROM %s\r\n" % self.fromaddr) 
+        dprint(self.sock.recv(1024))
+        self.sock.send("TO %s\r\n" % user)
+        dprint(self.sock.recv(1024))
+        self.sock.send("FILE %s\r\n" % self.filename) 
+        dprint(self.sock.recv(1024))
         self.sock.send("SIZE %i %i\r\n" % (self.filesize, self.filesize))
-        print self.sock.recv(1024)       
-        
+        dprint(self.sock.recv(1024))
         self.sock.send("DATA\r\n")
-        print self.sock.recv(1024)
+        dprint(self.sock.recv(1024))
 
-        #now its time to pfusch
         i = 0
-        cb(0.0)
+        if not self.executeCallbacks(0.0):
+            return
         while i < self.filesize:           
             i += self.sock.send(self.fileobj.read(1024)) #eh? error ones?
-            cb(i/float(self.filesize))
+            if not self.executeCallbacks(i/float(self.filesize)):
+                return
 
         self.sock.send("\r\n") 
-        print self.sock.recv(1024)
+        dprint(self.sock.recv(1024))
         self.sock.send("QUIT \r\n")
-        print self.sock.recv(1024)
+        dprint(self.sock.recv(1024))
         
         
-
 if __name__ == '__main__':
     def cb(progress): pass
     #def cb(p):
